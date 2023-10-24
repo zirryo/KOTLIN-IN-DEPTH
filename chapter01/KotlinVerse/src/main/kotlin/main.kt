05e9589d631bb5bb746137dff492b32c04e28c3d
Jihyo <jh04187@gmail.com>
2023-10-24 09:25:22 +0900

    chapter01 - 코틀린 프로젝트 구성

---
+++ chapter01/KotlinVerse/src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Hello, KotlinVerse!")
+}
+
+/* 프로젝트의 계층 구조
+src : 프로젝트에 포함된 소스 파일의 루트
+main/kotlin : 프로젝트의 코틀린 소스코드
+main/test : 프로젝트를 테스트하기 위란 테스트의 코틀린 소스코드
+resources : 프로그램이나 테스트에서 사용할 리소스 파일
+out : 컴파일러가 생성한 바이트코드. 코드를 한 번 이상 빌드하면 디렉토리가 생성됨.
+ */
+
+/* REPL
+- 인텔리제이 코틀린 플러그인이 제공하는 대화형 셸. 즉시 프로그램 실행을 평가할 수 있음.
+- 읽기(read) / 평가(evaluate) / 출력 루프(print loop) 의 약자
+- Tools > Kotlin > Kotlin REPL
+- 코드를 타이핑 한 후 command + enter 를 눌러서 코드 조각을 실행함.
+ */
